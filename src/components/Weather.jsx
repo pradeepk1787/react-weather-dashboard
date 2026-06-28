0399e3649c58cfcf79f18921a30e02d94ec67e22
pradeepk1787 <pradeepkamble1787@gmail.com>
2026-06-28 21:09:01 +0530

    Build weather dashboard with geolocation and API integration

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Weather() {
   //State declarations
@@ -30,6 +30,12 @@ function Weather() {
     fetchWeather(trimmedCity);
   };
 
+  //handleKeyDown()
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") handleSearch();
+  };
+
+  //Helper functions
   //fetchWeather
   const fetchWeather = async (cityName) => {
     try {
@@ -39,48 +45,154 @@ function Weather() {
       //Set loading to true
       setLoading(true);
 
-      //fetch weather data
-      const response = await fetch("");
+      const coordinates = await getCoordinates(cityName);
 
-      //handle if error in fetch data
-      if (!response.ok) throw new Error("Failed to get weather data");
+      const weatherData = await getCurrentWeather(
+        coordinates.latitude,
+        coordinates.longitude,
+        cityName
+      );
 
-      //parse JSON
-      const data = await response.json();
-
-      //set weather data
-      setWeather({
-        city: cityName,
-        temperature: data.temperature,
-        humidity: data.humidity,
-        wind: data.wind,
-        condition: data.condition,
-      });
+      setWeather(weatherData);
+      setCity("");
     } catch (error) {
       setError(error.message);
     } finally {
       setLoading(false);
     }
   };
-  //getCurrentLocationWeather()
 
-  //handleKeyDown()
-  const handleKeyDown = (e) => {
-    if (e.key === "Enter") handleSearch();
+  //get Current Location Weather
+  const getCurrentLocationWeather = () => {
+    if (!navigator.geolocation) {
+      fetchWeather("Mumbai");
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      async (position) => {
+        const { latitude, longitude } = position.coords;
+
+        try {
+          const weatherData = await getCurrentWeather(
+            latitude,
+            longitude,
+            "Current Location"
+          );
+
+          setWeather(weatherData);
+        } catch (error) {
+          //Silent fallback
+          fetchWeather("Mumbai");
+        }
+      },
+
+      () => {
+        // User denied permission
+        fetchWeather("Mumbai");
+      }
+    );
   };
+
+  //get weather condition
+  const getWeatherCondition = (code) => {
+    switch (code) {
+      case 0:
+        return "Clear Sky";
+
+      case 1:
+      case 2:
+        return "Partly Cloudy";
+
+      case 3:
+        return "Overcast";
+
+      case 61:
+        return "Rain";
+
+      case 95:
+        return "Thunderstorm";
+
+      default:
+        return "Unknown";
+    }
+  };
+
+  const getCoordinates = async (cityName) => {
+    //fetch city coordinates
+    const response = await fetch(
+      `https://geocoding-api.open-meteo.com/v1/search?name=${cityName}&count=1`
+    );
+
+    //handle if error in fetch city coordinates
+    if (!response.ok) throw new Error("Failed to get city coordinates");
+
+    const data = await response.json();
+
+    if (!data.results || data.results.length === 0) {
+      throw new Error("City not found");
+    }
+
+    return data.results[0];
+  };
+
+  const getCurrentWeather = async (latitude, longitude, cityName) => {
+    //fetch weather data
+    const response = await fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,wind_speed_10m,weather_code`
+    );
+
+    //handle if error in fetch data
+    if (!response.ok) throw new Error("Failed to get weather data");
+
+    //parse JSON
+    const data = await response.json();
+
+    //return weather data
+    return {
+      city: cityName,
+      temperature: data.current.temperature_2m,
+      humidity: data.current.relative_humidity_2m,
+      wind: data.current.wind_speed_10m,
+      condition: getWeatherCondition(data.current.weather_code),
+    };
+  };
+
+  //Use Effect
+  useEffect(() => {
+    getCurrentLocationWeather();
+  }, []);
 
   return (
     <div className="container">
       <h1>Weather Dashboard</h1>
+      <div className="search-box">
+        <input
+          type="text"
+          placeholder="City Name"
+          value={city}
+          onChange={handleCityChange}
+          onKeyDown={handleKeyDown}
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
+      {loading && <h2 className="loading">Loading...</h2>}
 
-      <input
-        type="text"
-        placeholder="City Name"
-        value={city}
-        onChange={handleCityChange}
-        onKeyDown={handleKeyDown}
-      />
-      <button onClick={handleSearch}>Search</button>
+      {error && <h2 className="error">{error}</h2>}
+
+      {weather && (
+        <div className="card">
+          <h2>{weather.city}</h2>
+
+          <p>🌡 Temperature: {weather.temperature}°C</p>
+
+          <p>💧 Humidity: {weather.humidity}%</p>
+
+          <p>🌬 Wind Speed: {weather.wind} km/h</p>
+
+          <p>☁️ Condition: {weather.condition}</p>
+        </div>
+      )}
     </div>
   );
 }
